136ab8547530ded3409ff354e5280d022aaa7460
Jamie Kyle <me@thejameskyle.com>
2024-07-03 10:42:54 -0700

    fun stuff

--- example/index.tsx
+++ example/index.tsx
@@ -1,3 +1,4 @@
+import confetti from "canvas-confetti"
 import { tinykeys } from "../src/tinykeys"
 
 tinykeys(window, {
@@ -9,5 +10,52 @@ tinykeys(window, {
 	},
 	"$mod+KeyU": () => {
 		alert("Either 'Control+u' or 'Meta+u' were pressed")
+	},
+})
+
+const KonamiCode = [
+	"ArrowUp",
+	"ArrowUp",
+	"ArrowDown",
+	"ArrowDown",
+	"ArrowLeft",
+	"ArrowRight",
+	"ArrowLeft",
+	"ArrowRight",
+	"KeyB",
+	"KeyA",
+	"Enter",
+].join(" ")
+
+tinykeys(window, {
+	[KonamiCode]: () => {
+		const duration = 15 * 1000
+		const animationEnd = Date.now() + duration
+		const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 }
+
+		function randomInRange(min: number, max: number) {
+			return Math.random() * (max - min) + min
+		}
+
+		const interval = setInterval(() => {
+			const timeLeft = animationEnd - Date.now()
+
+			if (timeLeft <= 0) {
+				return clearInterval(interval)
+			}
+
+			const particleCount = 50 * (timeLeft / duration)
+			// since particles fall down, start a bit higher than random
+			confetti({
+				...defaults,
+				particleCount,
+				origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+			})
+			confetti({
+				...defaults,
+				particleCount,
+				origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+			})
+		}, 250)
 	},
 })
